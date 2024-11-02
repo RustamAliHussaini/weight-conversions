d2aad3f2ad3cab13bc157ab793cd3fd9b4444bd8
RustamAliHussaini <RustamAliHussaini@users.noreply.github.com>
2024-11-02 10:33:27 +0000

    Fix styling

--- src/Weight.php
+++ src/Weight.php
@@ -2,21 +2,17 @@
 
 namespace RustamAliHussaini\WeightConversions;
 
-class Weight {
-
+class Weight
+{
     public static function kilograms(float $kilograms): self
     {
         return new static($kilograms);
     }
 
-    public function __construct( protected float $kilograms)
-    {
-
-    }
+    public function __construct(protected float $kilograms) {}
 
     public function toPounds(): float
     {
         return $this->kilograms * 2.2046;
     }
-
 }
