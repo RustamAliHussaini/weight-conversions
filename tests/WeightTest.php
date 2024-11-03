fb92a848b3d5f8ea1082f3c28a29bc8c3f5c17c8
RustamAliHussaini <101309331+RustamAliHussaini@users.noreply.github.com>
2024-11-03 09:31:59 +0430

    test text improved.

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -2,7 +2,7 @@
 
 use RustamAliHussaini\WeightConversions\Weight;
 
-it('it can convert kilograms to pounds', function () {
+it('it can convert kilograms to pounds correctly', function () {
     $pounds = Weight::kilograms(100)->toPounds();
 
     expect($pounds)->toEqual(220.46);
